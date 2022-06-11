661f4493bc7cf95483c7734355ef0e9bee23cedd
cater20 <onyangoketta@gmail.com>
2022-06-11 16:41:19 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+ 
+ var customerName = 'bob';
+ 
+//function customerName(bob) {
+    //return bob;
+  
+//}
+
+    function upperCaseCustomerName(){
+      customerName= customerName.toUpperCase () ;
+return customerName;
+    }
+
+
+
+function setBestCustomer() {
+  console.log( bestCustomer='not bob');
+}
+
+function overwriteBestCustomer(){
+    console.log(bestCustomer='maybe bob');
+}
+
+const leastFavoriteCustomer ='name';
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'new name';
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -38,3 +38,4 @@ describe('Fix the Scope', function() {
     });
   });
 });
+
